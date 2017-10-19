281e819ac9a4d5b79bde3c5ae2f6cb64318e9b25
belizdonmez <donmezbeliz.yp@gmail.com>
2017-10-19 13:35:55 +0300

    Add files via upload

---
+++ chess.java
@@ -0,0 +1,16 @@
+package satodev;
+
+import java.util.ArrayList;
+
+public class chess {
+
+	static String[][] tahta = new String[8][8];
+ public static void main(String[] args) {
+ for(int i=0; i<8;i++){
+		System.out.println(" ");
+		for(int j=0; j<8;j++){
+			System.out.print("|" + "B" + "|");
+		}
+		    System.out.println(" ");
+		} //fori
+}}
